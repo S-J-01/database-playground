deaaba29698283b6924139f30b92e8fdf110280b
Shreyas Jaltare <shreyasjaltare@gmail.com>
2025-01-18 23:51:21 +0530

    added addresses table with a foreign key and also added prettier

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-import express from 'express'
-const app = express()
-app.use(express.json())
+import express from "express";
+const app = express();
+app.use(express.json());
 
-import { Client } from 'pg'
-import dotenv from 'dotenv'
-dotenv.config()
+import { Client } from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
-
- 
 // const pgClient = new Client({
 //   host: process.env.HOST,
 //   port: 5432,
@@ -17,33 +15,44 @@ dotenv.config()
 //   ssl:true
 // })
 
-const pgClient = new Client(process.env.CONNECTION_STRING)
+const pgClient = new Client(process.env.CONNECTION_STRING);
 
+pgClient.connect();
 
-   pgClient.connect()
+app.post("/signup", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const email = req.body.email;
 
- 
-   app.post('/signup', async (req,res)=>{
-    const username = req.body.username
-    const password = req.body.password
-    const email = req.body.email
+  const city = req.body.city;
+  const country = req.body.country;
+  const street = req.body.street;
+  const pincode = req.body.pincode;
 
-    // const city = req.body.city
-    // const country = req.body.country
-    // const street = req.body.street
-    // const pincode = req.body.pincode
+  const insertQuery = `INSERT INTO users (username , email, password) VALUES($1, $2, $3) RETURNING id`;
 
+  const response = await pgClient.query(insertQuery, [
+    username,
+    email,
+    password,
+  ]);
 
-   const insertQuery = `INSERT INTO users (username , email, password) VALUES($1, $2, $3)`
+  const userId = response.rows[0].id;
 
-   const response = await pgClient.query(insertQuery,[username , email, password])
+  const insertAddressQuery = `INSERT INTO addresses (city, country, street, pincode , user_id) VALUES($1, $2, $3, $4, $5)`;
 
-   console.log(response)
-   res.json({
-    message:"Signed up successfully"
-   })
+  const responseAddressQuery = await pgClient.query(insertAddressQuery, [
+    city,
+    country,
+    street,
+    pincode,
+    userId,
+  ]);
 
-   } )
+  console.log(response);
+  res.json({
+    message: "Signed up successfully",
+  });
+});
 
-   app.listen(3000)
-
+app.listen(3000);
